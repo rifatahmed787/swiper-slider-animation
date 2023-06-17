cb5655034a219565fae63810a6de6900a9bc9b5d
weerotheme <weerotheme1@gmail.com>
2023-06-17 17:46:57 +0600

    text animation by Akash

--- src/Pages/Home/HomeVarient1/Banner.jsx
+++ src/Pages/Home/HomeVarient1/Banner.jsx
@@ -14,26 +14,26 @@ import SwiperCore, {
 import bannerImg1 from "../../../assets/images/banner/banner-1.png";
 import bannerImg2 from "../../../assets/images/banner/banner-2.png";
 import bannerImg3 from "../../../assets/images/banner/banner-3.png";
-import "./Banner.css"; // Import your custom CSS file for Banner component
 
 // Import Swiper modules
 SwiperCore.use([Autoplay, EffectFade, Navigation, Pagination]);
 
 const Banner = () => {
-  const [slideIndex, setSlideIndex] = useState(0);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const textRef = useRef(null);
 
   const handleSlideChange = (swiper) => {
     const currentIndex = swiper.activeIndex;
-    setSlideIndex(currentIndex);
+    setActiveSlideIndex(currentIndex);
+
     if (textRef.current) {
-      textRef.current.classList.add("bannerAnimation");
+      textRef.current.classList.add("banner-zoom");
     }
   };
 
   const handleTransitionEnd = () => {
     if (textRef.current) {
-      textRef.current.classList.remove("bannerAnimation");
+      textRef.current.classList.remove("banner-zoom");
     }
   };
 
@@ -55,34 +55,38 @@ const Banner = () => {
             <div className="bannerBG-1 md:h-[75vh] h-screen flex items-end">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {/* Left Side: Text Area */}
-                <div
-                  className={`flex py-10 items-center justify-center md:justify-start px-10 md:px-20 ${
-                    slideIndex === 0 ? "slide-in-right" : ""
-                  }`}
-                >
+                <div className="flex py-10 items-center justify-center md:justify-start px-10 md:px-20">
                   <div>
                     <h3
                       ref={textRef}
-                      className="text-white font-light text-xl md:text-2xl text-center md:text-start"
+                      className={`text-white font-light text-xl md:text-2xl text-center md:text-start ${
+                        activeSlideIndex === 0 ? "banner-zoom" : ""
+                      }`}
                     >
                       Welcome to Werro Dental Clinic
                     </h3>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4 ${
+                        activeSlideIndex === 0 ? "banner-zoom" : ""
+                      }`}
                     >
                       Let's Perfect Smile
                     </h1>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 ${
+                        activeSlideIndex === 0 ? "banner-zoom" : ""
+                      }`}
                     >
                       Caring with your teeth
                     </h1>
 
                     <article
                       ref={textRef}
-                      className="md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight"
+                      className={`md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight ${
+                        activeSlideIndex === 0 ? "banner-zoom" : ""
+                      }`}
                     >
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Vero aspernatur voluptates pariatur assumenda voluptatem
@@ -132,34 +136,38 @@ const Banner = () => {
                   <img src={bannerImg2} alt="banner photo" className="w-full" />
                 </div>
                 {/* Right Side: Text Area */}
-                <div
-                  className={`flex order-1 md:order-2 py-10 items-center justify-center md:justify-end px-10 md:px-20 ${
-                    slideIndex === 1 ? "slide-in-right" : ""
-                  }`}
-                >
+                <div className="flex order-1 md:order-2 py-10 items-center justify-center md:justify-end px-10 md:px-20">
                   <div className="md:pl-10">
                     <h3
                       ref={textRef}
-                      className="text-white font-light text-xl md:text-2xl text-center md:text-start"
+                      className={`text-white font-light text-xl md:text-2xl text-center md:text-start ${
+                        activeSlideIndex === 1 ? "banner-zoom" : ""
+                      }`}
                     >
                       Welcome to Werro Dental Clinic
                     </h3>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4 ${
+                        activeSlideIndex === 1 ? "banner-zoom" : ""
+                      }`}
                     >
                       Let's Perfect Smile
                     </h1>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 ${
+                        activeSlideIndex === 1 ? "banner-zoom" : ""
+                      }`}
                     >
                       Caring with your teeth
                     </h1>
 
                     <article
                       ref={textRef}
-                      className="md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight"
+                      className={`md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight ${
+                        activeSlideIndex === 1 ? "banner-zoom" : ""
+                      }`}
                     >
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Vero aspernatur voluptates pariatur assumenda voluptatem
@@ -201,34 +209,38 @@ const Banner = () => {
             <div className="bannerBG-1 md:h-[75vh] h-screen flex items-end">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {/* Left Side: Text Area */}
-                <div
-                  className={`flex py-10 items-center justify-center md:justify-start px-10 md:px-20 ${
-                    slideIndex === 2 ? "slide-in-right" : ""
-                  }`}
-                >
+                <div className="flex py-10 items-center justify-center md:justify-start px-10 md:px-20">
                   <div>
                     <h3
                       ref={textRef}
-                      className="text-white font-light text-xl md:text-2xl text-center md:text-start"
+                      className={`text-white font-light text-xl md:text-2xl text-center md:text-start ${
+                        activeSlideIndex === 2 ? "banner-zoom" : ""
+                      }`}
                     >
                       Welcome to Werro Dental Clinic
                     </h3>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 md:w-3/4 ${
+                        activeSlideIndex === 2 ? "banner-zoom" : ""
+                      }`}
                     >
                       Let's Perfect Smile
                     </h1>
                     <h1
                       ref={textRef}
-                      className="text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5"
+                      className={`text-2xl md:text-6xl text-center md:text-start font-bold bg-third text-white p-3 md:p-5 my-3 md:my-5 ${
+                        activeSlideIndex === 2 ? "banner-zoom" : ""
+                      }`}
                     >
                       Caring with your teeth
                     </h1>
 
                     <article
                       ref={textRef}
-                      className="md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight"
+                      className={`md:text-white text-black text-base text-center md:text-start md:text-xl font-extralight ${
+                        activeSlideIndex === 2 ? "banner-zoom" : ""
+                      }`}
                     >
                       LHEllo Akashd
                     </article>
@@ -258,11 +270,7 @@ const Banner = () => {
                   </div>
                 </div>
                 {/* Right side: Banner Slider Image */}
-                <div
-                  className={`md:mt-14 ${
-                    slideIndex === 2 ? "slide-in-right" : ""
-                  }`}
-                >
+                <div>
                   <img src={bannerImg3} alt="banner photo" className="w-full" />
                 </div>
               </div>
